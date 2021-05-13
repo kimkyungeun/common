c196f060e1c425457a8a742e62f978359b3ee76e
kimkyungeun <kimkyungeun@kakao.com>
2021-05-14 00:01:47 +0900

    Create Original_sys.js

---
+++ front/JavaScript/Original/Original_sys.js
@@ -0,0 +1,19 @@
+/**
+ * @file comm_sys.js
+ * <p>comm_sys.js : 공통 </p>
+ */
+
+/*********************************************************
+ * <b>FUNCTION</b>
+ * <p>
+ * Function Name : sysfn_<br />
+ * Description : <br />
+ * </p>
+ *
+ * <p>[수정이력]</p>
+ * <ul>
+ * <li>2021.05.10 / 김경은(kimkyungeun@kakao.com) / 최초작성</li>
+ * </ul>
+ *********************************************************/
+function sysfn_() {
+}
