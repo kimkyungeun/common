ad01b224b55c921c1adaeee3bdbad10cd7f99d0a
kimkyungeun <kimkyungeun@kakao.com>
2021-05-14 00:00:54 +0900

    Create Angular_utl.js

---
+++ front/JavaScript/Angular/Angular_utl.js
@@ -0,0 +1,19 @@
+/**
+ * @file comm_utl.js
+ * <p>comm_utl.js : 공통 </p>
+ */
+
+/*********************************************************
+ * <b>FUNCTION</b>
+ * <p>
+ * Function Name : utlfn_<br />
+ * Description : <br />
+ * </p>
+ *
+ * <p>[수정이력]</p>
+ * <ul>
+ * <li>2021.05.10 / 김경은(kimkyungeun@kakao.com) / 최초작성</li>
+ * </ul>
+ *********************************************************/
+function utlfn_() {
+}
