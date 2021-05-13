afa8e5c8d2e193a882ed29dd6ad187eff3ffa5fa
kimkyungeun <kimkyungeun@kakao.com>
2021-05-14 00:01:37 +0900

    Create Original_frm.js

---
+++ front/JavaScript/Original/Original_frm.js
@@ -0,0 +1,19 @@
+/**
+ * @file comm_frm.js
+ * <p>comm_frm.js : 공통 프레임</p>
+ */
+
+/*********************************************************
+ * <b>FUNCTION</b>
+ * <p>
+ * Function Name : frmfn_screen<br />
+ * Description : 화면 처리<br />
+ * </p>
+ *
+ * <p>[수정이력]</p>
+ * <ul>
+ * <li>2021.05.10 / 김경은(kimkyungeun@kakao.com) / 최초작성</li>
+ * </ul>
+ *********************************************************/
+function frmfn_screen(obj, path) {
+}
