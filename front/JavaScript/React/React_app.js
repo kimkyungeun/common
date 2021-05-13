f5fef60e90c78507bb61e1cc6ec6190f18d6c809
kimkyungeun <kimkyungeun@kakao.com>
2021-05-14 00:01:54 +0900

    Create React_app.js

---
+++ front/JavaScript/React/React_app.js
@@ -0,0 +1,19 @@
+/**
+ * @file comm_app.js
+ * <p>comm_app.js : 공통 </p>
+ */
+
+/*********************************************************
+ * <b>FUNCTION</b>
+ * <p>
+ * Function Name : appfn_<br />
+ * Description : <br />
+ * </p>
+ *
+ * <p>[수정이력]</p>
+ * <ul>
+ * <li>2021.05.10 / 김경은(kimkyungeun@kakao.com) / 최초작성</li>
+ * </ul>
+ *********************************************************/
+function appfn_() {
+}
