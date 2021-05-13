3546265621e960aa3f8f7ef1a1328c2325fa98a7
kimkyungeun <kimkyungeun@kakao.com>
2021-05-14 00:02:01 +0900

    Create React_grd.js

---
+++ front/JavaScript/React/React_grd.js
@@ -0,0 +1,19 @@
+/**
+ * @file comm_grd.js
+ * <p>comm_grd.js : 공통 </p>
+ */
+
+/*********************************************************
+ * <b>FUNCTION</b>
+ * <p>
+ * Function Name : grdfn_<br />
+ * Description : <br />
+ * </p>
+ *
+ * <p>[수정이력]</p>
+ * <ul>
+ * <li>2021.05.10 / 김경은(kimkyungeun@kakao.com) / 최초작성</li>
+ * </ul>
+ *********************************************************/
+function grdfn_() {
+}
